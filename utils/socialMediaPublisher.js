a92ddf03a4dce8046115352eb5bcd5410617a929
syaifulazham <120453871+syaifulazham@users.noreply.github.com>
2024-02-28 11:56:23 +0800

    Update socialMediaPublisher.js

--- utils/socialMediaPublisher.js
+++ utils/socialMediaPublisher.js
@@ -1,12 +1,11 @@
 exports.publishToFacebook = (postContent) => {
-    // Logic to publish postContent to Facebook
-  };
-  
-  exports.publishToInstagram = (postContent) => {
-    // Logic to publish postContent to Instagram
-  };
-  
-  exports.publishToTikTok = (postContent) => {
-    // Logic to publish postContent to TikTok
-  };
-  
+  // Logic to publish postContent to Facebook
+};
+
+exports.publishToInstagram = (postContent) => {
+  // Logic to publish postContent to Instagram
+};
+
+exports.publishToTikTok = (postContent) => {
+  // Logic to publish postContent to TikTok
+};
